e3029e491082ba397513e4eb59bf8b1958ce9653
Tyler Hueter <tylerhueter08@gmail.com>
2019-10-16 16:43:54 -0400

    connected in mongoDB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
+
+const mongoURI = 'mongodb+srv://tyleradmin:adminpassword@cluster0-oeg0z.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err)
+})
 
 app.get('/', (req, res) => {
     res.send('Hi There!')
