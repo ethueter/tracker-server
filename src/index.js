e33f7c07932714f4cf12e536116d255e72091bf4
Tyler Hueter <tylerhueter08@gmail.com>
2019-10-22 20:51:35 -0400

    jwt/bcrypt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+require('./models/User')
 const express = require('express');
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth')
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
 
 const mongoURI = 'mongodb+srv://tyleradmin:adminpassword@cluster0-oeg0z.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(mongoURI, {
@@ -17,8 +24,8 @@ mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongo', err)
 })
 
-app.get('/', (req, res) => {
-    res.send('Hi There!')
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
